92bb66772498b2fd78f9a6bbaf5e66f987b16ead
Brian J. Enquist <benquist@email.arizona.edu>
2015-03-03 08:58:58 -0700

    updates with devtools installation

--- R_CHAMBASA.R
+++ R_CHAMBASA.R
@@ -2,7 +2,10 @@
 ######################
 # Brian J. Enquist
 # R for CHAMBASA - Peru Campaign
+# 
 #####################
 
+#
 # install postgreSQL
 install.packages("RPostgreSQL")
+install.packages("devtools")
