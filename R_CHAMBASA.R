cbb9e6339a0e312b92ff02acf693949695b7b118
Brian J. Enquist <benquist@email.arizona.edu>
2015-03-03 09:46:27 -0700

    additional edits

--- R_CHAMBASA.R
+++ R_CHAMBASA.R
@@ -2,10 +2,47 @@
 ######################
 # Brian J. Enquist
 # R for CHAMBASA - Peru Campaign
-# 
+# Accessing CHAMBASA data via database connection
 #####################
 
-#
 # install postgreSQL
 install.packages("RPostgreSQL")
 install.packages("devtools")
+library(devtools)
+library(gemtraits)
+
+# first thing that you do is to create a connection with the database
+# type con = connect_gemtraits_db()
+con = connect_gemtraits_db()
+
+#queries run by commands in the R file db_queries.r
+trees_crowns = get_trees_with_crowns(con)
+LeafChem = get_leaf_chemistry(con)
+WoodDensity = get_wood_density(con)
+LeafParts = get_leaf_parts(con)
+
+head(LeafParts)
+head(LeafChem )
+class(LeafChem)
+names(LeafChem)
+dim(LeafChem)
+
+#summary(a)           # gives min, max, mean, median, 1st & 3rd quartiles
+#min(a); max(a)       # }
+#range(a)             # } self-explanatory
+#mean(a); median(a)   # }
+#sd(a); mad(a)        # standard deviation, median absolute deviation
+#IQR(a)               # interquartile range
+#quantile(a)          # quartiles (by default)
+#quantile(a, c(1, 3)/4)  # specific percentiles (25% & 75% in this case)
+
+#summarize data
+hist(LeafChem$n15_delta)
+hist(LeafChem$chl_b)
+hist(LeafChem$chl_b)
+
+#save as csv
+write.csv(trees_crowns, file="tree_crowns.csv")
+
+
+
