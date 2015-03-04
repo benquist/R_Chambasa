cc1bcd83c3f8384bf4e3c6621a8b90f4474fce40
Brian J. Enquist <benquist@email.arizona.edu>
2015-03-04 09:09:58 -0700

    updates

--- R_CHAMBASA.R
+++ R_CHAMBASA.R
@@ -5,7 +5,7 @@
 # Accessing CHAMBASA data via database connection
 #####################
 
-# install postgreSQL
+# install postgreSQL, install.packages("foo", dependencies=...)
 install.packages("RPostgreSQL")
 install.packages("devtools")
 library(devtools)
@@ -21,11 +21,17 @@ LeafChem = get_leaf_chemistry(con)
 WoodDensity = get_wood_density(con)
 LeafParts = get_leaf_parts(con)
 
+#data summar
 head(LeafParts)
 head(LeafChem )
 class(LeafChem)
 names(LeafChem)
 dim(LeafChem)
+
+#summarize number of observed datapoints = True and NAs = False
+table(LeafChem$plot_code, !is.na(LeafChem$n_percent))
+unique(LeafChem$plot_code)
+
 
 #summary(a)           # gives min, max, mean, median, 1st & 3rd quartiles
 #min(a); max(a)       # }
@@ -60,9 +66,18 @@ myplot <- ggplot(data=LeafChem, aes(x = p, y = n))
 summary(myplot)
 myplot + geom_point() 
 
+#photosynthesis we need to subset the 
+# sunleaves <- subset(pm_type
+
 #color bivariate plot by plot
-ggplot(LeafChem, aes(n, p, color=plot_code)) + geom_point()
+ggplot(LeafChem, aes(log(n), log(p), color=plot_code)) + geom_point()
 ggplot(LeafChem, aes(lma, n_percent, color=plot_code)) + geom_point()
+ggplot(LeafChem, aes(lma, p_corrected_percent, color=plot_code)) + geom_point()
+
+ggplot(LeafChem, aes(log(photosynthesis), log(n_percent), color=plot_code)) + geom_point()
+
+ggplot(LeafChem, aes(lma, n_percent, color=plot_code)) + geom_point()
+
 
 #boxplot of trait values
 library(MASS)
@@ -72,5 +87,35 @@ ggplot(LeafChem, aes(factor(plot_code), p))+ geom_boxplot()
 ggplot(iris, aes(n, p, color=plot_code)) + 
   geom_point() +
   facet_grid(plot_code ~ .) 
+
+
+# From Lisa
+library(smatr)
+library(plyr)
+#join <- read.csv("photo_db_join.csv")
+
+Amax<-subset(LeafChem, (pm_type %in% "AMAX"))
+Amax_sun<-subset(Amax, (sun_shade %in% "SUN"))
+
+tests = list()
+for (p in unique(Amax_sun$plot_code)) {
+  this_plot = subset(Amax_sun, plot_code == p)
+  print(paste("Results for Plot ",p))
+  test1<-sma(photosynthesis~n_percent,data = this_plot,log="xy",na.omit=T)
+  tests[[p]] = test1
+  plot(test1)
+  summary(test1)
+}
+
+Amax_shade<-subset(Amax, (sun_shade %in% "SHADE"))
+tests = list()
+for (p in unique(Amax_shade$plot_code)) {
+  this_plot = subset(Amax_shade, plot_code == p)
+  print(paste("Results for Plot ",p))
+  test1<-sma(photosynthesis~n_percent,data = this_plot,log="xy",na.omit=T)
+  tests[[p]] = test1
+  plot(test1)
+  summary(test1)
+}
 
 
